38b69432284a5f9851afbfaa5e60595a98d126e5
ondrej337 <40807038+ondrej337@users.noreply.github.com>
2021-03-14 20:55:56 +0100

    Update daikin.sh

--- daikin.sh
+++ daikin.sh
@@ -3,6 +3,6 @@ python3 /var/lib/postgresql/daikin/daikin_wrapper.py > /var/lib/postgresql/daiki
 
 sleep 3 
 
-cat /var/lib/postgresql/daikin/daikin_load.csv | psql -d kovals -c "\COPY daikin.daikin_wrap(adapter_model,outdoor_temperature,tank_temperature,leaving_water_temperature,power_state,power_consumption,tank_consumption) FROM '/var/lib/postgresql/daikin/daikin_load.csv'
+cat /var/lib/postgresql/daikin/daikin_load.csv | psql -d postgres -c "\COPY daikin.daikin_wrap(adapter_model,outdoor_temperature,tank_temperature,leaving_water_temperature,power_state,power_consumption,tank_consumption) FROM '/var/lib/postgresql/daikin/daikin_load.csv'
 DELIMITER ';'"
 
